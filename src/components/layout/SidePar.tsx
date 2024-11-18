eea57b30c8b538f394ff4624fe9662218c2773bd
montaser <montasergohar@gmail.com>
2024-11-18 15:56:52 +0200

    side par responsive

--- src/components/layout/SidePar.tsx
+++ src/components/layout/SidePar.tsx
@@ -2,16 +2,33 @@ import { useState } from "react";
 import logo from "../../assets/Group 14.png";
 import { LuLayoutDashboard, LuStethoscope } from "react-icons/lu";
 import { MdBurstMode, MdCreditScore, MdDomain, MdEngineering, MdOutlineSchool, MdOutlineStorefront } from "react-icons/md";
-import { CiSettings } from "react-icons/ci";
+import { CiMenuFries, CiSettings } from "react-icons/ci";
+import { IoMdClose } from "react-icons/io";
 
 const SidePar = () => {
   const [active, setActive] = useState("Dashboard");
+  const [isOpen, setIsOpen] = useState(false);
+  console.log(isOpen);
+  
 
   return ( 
-    <div className="fixed inset-y-0 overflow-auto scrollbar scrollbar-thumb-blue-500  sidebar-scrollbar">
+    <>
+    <div className="py-4 w-fit absolute top-3 left-3   z-50 block md:hidden  cursor-pointer " onClick={() => setIsOpen((prev) => !prev)}>
+        {isOpen ? (
+          <CiMenuFries size={30} className="w-7 h-7  z-50 cursor-pointer" />
+        ) : (
+          <IoMdClose  size={30} className="w-7 h-7 z-50 cursor-pointer fixed " />
+        )}
+    </div>
+    <div
+  className={`fixed top-0 left-0 h-screen w-fit overflow-y-auto scrollbar scrollbar-thumb-blue_maincolor sidebar-scrollbar transform transition-transform duration-300 ease-in-out ${
+    isOpen ? "-translate-x-full" : "translate-x-0"
+  }`}
+>
+
       <div className="w-[236px] shadow-sm border min-h-screen   ">
         <div className="flex justify-center py-6 shadow-sm     ">
-          <img src={logo} alt="Logo"  className="w-[150px]"/>
+          <img src={logo} alt="Logo"  className="w-[100px] md:[150px]" />
         </div>
 
         <div className="text-blue_maincolor text-[18px] font-inter font-semibold leading-[16px] break-words overflow-auto ">
@@ -141,6 +158,8 @@ const SidePar = () => {
         </div>
       </div>
     </div>
+    </>
+
   );
 };
 
