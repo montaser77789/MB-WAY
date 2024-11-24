884578d669101f4c5ff540e8ca3f366e63172859
montaser <montasergohar@gmail.com>
2024-11-24 21:30:02 +0200

    add chart

--- src/pages/MedicalSection/Medical.tsx
+++ src/pages/MedicalSection/Medical.tsx
@@ -11,12 +11,12 @@ import {
 import backgroundImage from "../../assets/Ellipse 38.png";
 
 const chartData = [
-  { name: "Page A", uv: 4000, pv: 2400, amt: 2400 },
-  { name: "Page B", uv: 3000, pv: 1398, amt: 2210 },
-  { name: "Page C", uv: 2000, pv: 9800, amt: 2290 },
-  { name: "Page D", uv: 2780, pv: 3908, amt: 2000 },
-  { name: "Page E", uv: 1890, pv: 4800, amt: 2181 },
-  { name: "Page F", uv: 2390, pv: 3800, amt: 2500 },
+  { name: "sat",    uv: 4000, pv: 2400, amt: 2400 },
+  { name: "sun", uv: 3000, pv: 1398, amt: 2210 },
+  { name: "mon", uv: 2000, pv: 9800, amt: 2290 },
+  { name: "tue", uv: 2780, pv: 3908, amt: 2000 },
+  { name: "wed", uv: 1890, pv: 4800, amt: 2181 },
+  { name: "thu", uv: 2390, pv: 3800, amt: 2500 },
 ];
 
 const Medical = () => {
